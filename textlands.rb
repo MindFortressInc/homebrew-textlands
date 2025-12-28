12380a65035e8a00ed8fa7566dacb47583759eb4
Matt Rhodes <mattrhodes77@gmail.com>
2025-12-28 13:22:19 -0800

    Update to v0.1.1 - fix standalone binary imports

--- textlands.rb
+++ textlands.rb
@@ -4,23 +4,23 @@
 class Textlands < Formula
   desc "Play TextLands text adventures from your terminal"
   homepage "https://textlands.com"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/MindFortressInc/textlands-cli/releases/download/v#{version}/textlands-macos-arm64"
-      sha256 "e95543ccc125004b0e56a66caa70b3a70a1965bf795d4e666fa4815f602e6d22"
+      sha256 "f48c8127bfc9acfced523328931c557e927f6f85493e7bee50c953850ceaaecb"
     end
     on_intel do
       url "https://github.com/MindFortressInc/textlands-cli/releases/download/v#{version}/textlands-macos-x64"
-      sha256 "a2294d415c4ab1dfa6fd34443b6a2953382c2d12b5c5b1cbb52bb86229b6c0d2"
+      sha256 "31221f4f684037c6fce458ad6b2240546542245bcbe839c0e42798dc1837f251"
     end
   end
 
   on_linux do
     url "https://github.com/MindFortressInc/textlands-cli/releases/download/v#{version}/textlands-linux-x64"
-    sha256 "2d4b9663c26d312c27b13c3285dc0d6b3875a3d871e65eafb0a1c0a4ec1aa482"
+    sha256 "aa10d6dd90af5f93282bd7515b23aa279525437802830e6b3f36d416fd1f62bb"
   end
 
   def install
